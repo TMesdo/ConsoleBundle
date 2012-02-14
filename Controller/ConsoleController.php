2f602bb697f278214c57800c73a76ad4f072d62a
winzou <alexandre.bacco@gmail.com>
2012-02-14 11:46:34 +0100

    Handle the case when a command dump something instead of returning it

--- Controller/ConsoleController.php
+++ Controller/ConsoleController.php
@@ -68,11 +68,17 @@ class ConsoleController extends Controller
 
         $application = $this->getApplication($input);
         $application->setAutoExit(FALSE);
-        $application->run($input, $output);
+        
+        // Some commands (i.e. doctrine:query:dql) dump things out instead of returning a value
+        // Looks like a hack, but no way to do this differently
+		ob_start();
+		$application->run($input, $output);
+		$dumped = ob_get_contents(); // We catch it and concatenate it to the output
+		ob_end_clean();
 
         return $this->render('CoreSphereConsoleBundle:Console:result.' . $_format . '.twig', array(
-            'input' => $command,
-            'output' => $output->getBuffer(),
+            'input'       => $command,
+            'output'      => $output->getBuffer().$dumped, // Theorically, a command doesn't return and dump at the same time
             'environment' => $this->getKernel($input)->getEnvironment(),
         ));
     }
